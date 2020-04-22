744e1597ef1edf20d22dd95405ae9bb8293ff802
myhilmyhill <44314051+myhilmyhill@users.noreply.github.com>
2020-04-23 08:43:30 +0900

    separate main to Field

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,11 @@
 import * as PIXI from 'pixi.js';
+import { Field } from './model/field/field';
 import { InputPad } from './input-pad';
-import { Player } from './model/character/player';
-import { Coordinate } from './model/character/coordinate';
-import { MovingPlayer } from './model/character/moving-player';
 
 export class Main {
-  static vx = 0;
-  static vy = 0;
   static screenWidth = 512;
   static screenHeight = 720;
-  static isSlow = false;
-  static waittimeNextBullet = 0;
-  static protagonist: Player;
-  static bullets: MovingPlayer[];
-  static myBullets: MovingPlayer[];
+  static field: Field;
   static state: () => void;
 
   public static run(): void {
@@ -22,220 +14,45 @@ export class Main {
       height: this.screenHeight,
     });
     document.body.appendChild(app.view);
-
-    this.bullets = Array.from(
-      { length: 1000 },
-      () =>
-        new MovingPlayer(
-          Math.random() * this.screenWidth,
-          Math.random() * this.screenHeight,
-          10,
-          (x, y, radius, graphics) => {
-            graphics.beginFill(0xffffff).drawCircle(x, y, radius).endFill();
-            app.stage.addChild(graphics);
-          },
-          (self) => this.moveDiagonallyAndBounce(self),
-        ),
-    );
-    this.myBullets = Array.from(
-      { length: 30 },
-      () =>
-        new MovingPlayer(
-          0,
-          0,
-          5,
-          (x, y, radius, graphics) => {
-            graphics.beginFill(0x00ff00).drawCircle(x, y, radius).endFill();
-            app.stage.addChild(graphics);
-            graphics.visible = false;
-          },
-          (self) => (): void => {
-            self.y -= 10;
-            if (self.y < -self.radius) {
-              self.vanish();
-            }
-
-            // Hit enemy
-            for (const bullet of this.bullets) {
-              if (
-                bullet.isVisible &&
-                self.isVisible &&
-                Coordinate.isCollided(self.hitarea, bullet.hitarea)
-              ) {
-                bullet.vanish();
-                self.vanish();
-              }
-            }
-          },
-        ),
-    );
-    //Create the `cat` sprite
-    this.protagonist = new Player(0, 0, 32, (x, y, radius, graphics) => {
-      graphics.beginFill(0xff0000).drawCircle(x, y, radius).endFill();
-      app.stage.addChild(graphics);
-    });
+    this.field = new Field(app, this.screenWidth, this.screenHeight);
 
     InputPad.addButton(
       'z',
-      () => this.launchBullet(),
-      () => this.launchBullet(),
-    );
-    InputPad.addButton(
-      'a',
-      () => this.bullets.forEach((b) => b.show()),
-      () => this.bullets.forEach((b) => b.show()),
-    );
-    InputPad.addButton(
-      'x',
-      (): void => {
-        // Bullets will stop
-        this.bullets.forEach((b) => b.setFuncMoving(undefined));
-      },
-      (): void => {
-        // Bullets will move again but does not restore the velocity and direction
-        this.bullets.forEach((b) =>
-          b.setFuncMoving((self) => this.moveDiagonallyAndBounce(self)),
-        );
-      },
+      () => this.field.launchBullet(),
+      () => this.field.launchBullet(),
     );
     InputPad.addButton(
       'Shift',
-      (): void => {
-        this.isSlow = true;
-      },
-      (): void => {
-        this.isSlow = false;
-      },
+      (): void => this.field.toggleSlow(true),
+      (): void => this.field.toggleSlow(false),
     );
     InputPad.addButton(
       'ArrowLeft',
-      (): void => {
-        this.vx = -this.setMovingVelocity();
-        if (this.protagonist.x - this.protagonist.radius <= 0) {
-          this.protagonist.x = this.protagonist.radius;
-        }
-      },
-      (): void => {
-        this.vx = 0;
-      },
+      (): void => this.field.controlLeft(),
+      (): void => this.field.stopX(),
     );
     InputPad.addButton(
       'ArrowUp',
-      (): void => {
-        this.vy = -this.setMovingVelocity();
-        if (this.protagonist.y - this.protagonist.radius <= 0) {
-          this.protagonist.y = this.protagonist.radius;
-        }
-      },
-      (): void => {
-        this.vy = 0;
-      },
+      (): void => this.field.controlUp(),
+      (): void => this.field.stopY(),
     );
     InputPad.addButton(
       'ArrowRight',
-      (): void => {
-        this.vx = this.setMovingVelocity();
-        if (this.protagonist.x + this.protagonist.radius >= this.screenWidth) {
-          this.protagonist.x = this.screenWidth - this.protagonist.radius;
-        }
-      },
-      (): void => {
-        this.vx = 0;
-      },
+      (): void => this.field.controlRight(),
+      (): void => this.field.stopX(),
     );
     InputPad.addButton(
       'ArrowDown',
-      (): void => {
-        this.vy = this.setMovingVelocity();
-        if (this.protagonist.y + this.protagonist.radius >= this.screenHeight) {
-          this.protagonist.y = this.screenHeight - this.protagonist.radius;
-        }
-      },
-      (): void => {
-        this.vy = 0;
-      },
+      (): void => this.field.controlDown(),
+      (): void => this.field.stopY(),
     );
-
-    //Set the game state
-    this.state = this.move;
 
     //Start the game loop
     app.ticker.add(() => this.gameLoop());
   }
 
-  static setMovingVelocity(): number {
-    return (this.isSlow ? 3 : 6) / (this.vx * this.vy !== 0 ? 1.41421356 : 1);
-  }
-
-  static moveDiagonallyAndBounce(self: MovingPlayer): () => void {
-    // Captured variables
-    const circle = self.hitarea;
-    let vx = Math.random() + 1;
-    let vy = Math.random();
-
-    const invert = (): void => {
-      if (this.screenWidth <= circle.x + circle.radius) {
-        circle.x = this.screenWidth - circle.radius;
-        vx *= -1;
-      } else if (circle.x - circle.radius <= 0) {
-        circle.x = circle.radius;
-        vx *= -1;
-      }
-      if (this.screenHeight <= circle.y + circle.radius) {
-        circle.y = this.screenHeight - circle.radius;
-        vy *= -1;
-      } else if (circle.y - circle.radius <= 0) {
-        circle.y = circle.radius;
-        vy *= -1;
-      }
-    };
-    return (): void => {
-      circle.x += vx;
-      circle.y += vy;
-      invert();
-      this.determineHit(self);
-    };
-  }
-
-  static determineHit(bullet: MovingPlayer): void {
-    // Determine the protagonist is hit by bulltes
-    if (
-      bullet.isVisible &&
-      Coordinate.isCollided(this.protagonist.hitarea, bullet.hitarea)
-    ) {
-      bullet.vanish();
-    }
-  }
-
-  static launchBullet(): void {
-    if (this.waittimeNextBullet > 0) return;
-
-    let single = false;
-    for (const myBullet of this.myBullets) {
-      if (myBullet.isVisible) continue;
-      myBullet.show();
-      myBullet.x = this.protagonist.x + (single ? 10 : -10);
-      myBullet.y = this.protagonist.y;
-      if (single) return;
-      single = true;
-      this.waittimeNextBullet = 5;
-    }
-  }
-
   static gameLoop(): void {
-    this.state();
     InputPad.fire();
-    for (const bullet of this.bullets) {
-      bullet.move();
-    }
-    for (const myBullet of this.myBullets) {
-      myBullet.move();
-    }
-    this.waittimeNextBullet -= this.waittimeNextBullet > 0 ? 1 : 0;
-  }
-
-  static move(): void {
-    this.protagonist.x += this.vx;
-    this.protagonist.y += this.vy;
+    this.field.loop();
   }
 }
---
+++ src/model/field/field.ts
@@ -0,0 +1,191 @@
+import { Coordinate } from '../character/coordinate';
+import { MovingPlayer } from '../character/moving-player';
+import { Player } from '../character/player';
+import * as PIXI from 'pixi.js';
+
+export class Field {
+  protected protagonist: Player;
+  protected bullets: MovingPlayer[];
+  protected myBullets: MovingPlayer[];
+
+  protected screenHeight: number;
+  protected screenWidth: number;
+  protected waittimeNextBullet = 0;
+  protected vx = 0;
+  protected vy = 0;
+  protected isSlow = false;
+
+  constructor(
+    app: PIXI.Application,
+    screenWidth: number,
+    screenHeight: number,
+  ) {
+    this.screenWidth = screenWidth;
+    this.screenHeight = screenHeight;
+
+    this.bullets = Array.from(
+      { length: 1000 },
+      () =>
+        new MovingPlayer(
+          Math.random() * this.screenWidth,
+          Math.random() * this.screenHeight,
+          10,
+          (x, y, radius, graphics) => {
+            graphics.beginFill(0xffffff).drawCircle(x, y, radius).endFill();
+            app.stage.addChild(graphics);
+          },
+          (self) => this.moveDiagonallyAndBounce(self),
+        ),
+    );
+    this.myBullets = Array.from(
+      { length: 30 },
+      () =>
+        new MovingPlayer(
+          0,
+          0,
+          5,
+          (x, y, radius, graphics) => {
+            graphics.beginFill(0x00ff00).drawCircle(x, y, radius).endFill();
+            app.stage.addChild(graphics);
+            graphics.visible = false;
+          },
+          (self) => (): void => {
+            self.y -= 10;
+            if (self.y < -self.radius) {
+              self.vanish();
+            }
+
+            // Hit enemy
+            for (const bullet of this.bullets) {
+              if (
+                bullet.isVisible &&
+                self.isVisible &&
+                Coordinate.isCollided(self.hitarea, bullet.hitarea)
+              ) {
+                bullet.vanish();
+                self.vanish();
+              }
+            }
+          },
+        ),
+    );
+
+    this.protagonist = new Player(0, 0, 32, (x, y, radius, graphics) => {
+      graphics.beginFill(0xff0000).drawCircle(x, y, radius).endFill();
+      app.stage.addChild(graphics);
+    });
+  }
+
+  loop(): void {
+    this.protagonist.x += this.vx;
+    this.protagonist.y += this.vy;
+    for (const bullet of this.bullets) {
+      bullet.move();
+    }
+    for (const myBullet of this.myBullets) {
+      myBullet.move();
+    }
+    this.waittimeNextBullet -= this.waittimeNextBullet > 0 ? 1 : 0;
+  }
+
+  moveDiagonallyAndBounce(self: MovingPlayer): () => void {
+    // Captured variables
+    const circle = self.hitarea;
+    let vx = Math.random() + 1;
+    let vy = Math.random();
+
+    const invert = (): void => {
+      if (this.screenWidth <= circle.x + circle.radius) {
+        circle.x = this.screenWidth - circle.radius;
+        vx *= -1;
+      } else if (circle.x - circle.radius <= 0) {
+        circle.x = circle.radius;
+        vx *= -1;
+      }
+      if (this.screenHeight <= circle.y + circle.radius) {
+        circle.y = this.screenHeight - circle.radius;
+        vy *= -1;
+      } else if (circle.y - circle.radius <= 0) {
+        circle.y = circle.radius;
+        vy *= -1;
+      }
+    };
+    return (): void => {
+      circle.x += vx;
+      circle.y += vy;
+      invert();
+      this.determineHit(self);
+    };
+  }
+
+  determineHit(bullet: MovingPlayer): void {
+    // Determine the protagonist is hit by bulltes
+    if (
+      bullet.isVisible &&
+      Coordinate.isCollided(this.protagonist.hitarea, bullet.hitarea)
+    ) {
+      bullet.vanish();
+    }
+  }
+
+  // Controll the protagonist
+
+  launchBullet(): void {
+    if (this.waittimeNextBullet > 0) return;
+
+    let single = false;
+    for (const myBullet of this.myBullets) {
+      if (myBullet.isVisible) continue;
+      myBullet.show();
+      myBullet.x = this.protagonist.x + (single ? 10 : -10);
+      myBullet.y = this.protagonist.y;
+      if (single) return;
+      single = true;
+      this.waittimeNextBullet = 5;
+    }
+  }
+
+  setMovingVelocity(): number {
+    return (this.isSlow ? 3 : 6) / (this.vx * this.vy !== 0 ? 1.41421356 : 1);
+  }
+
+  controlLeft(): void {
+    this.vx = -this.setMovingVelocity();
+    if (this.protagonist.x - this.protagonist.radius <= 0) {
+      this.protagonist.x = this.protagonist.radius;
+    }
+  }
+
+  controlUp(): void {
+    this.vy = -this.setMovingVelocity();
+    if (this.protagonist.y - this.protagonist.radius <= 0) {
+      this.protagonist.y = this.protagonist.radius;
+    }
+  }
+
+  controlRight(): void {
+    this.vx = this.setMovingVelocity();
+    if (this.protagonist.x + this.protagonist.radius >= this.screenWidth) {
+      this.protagonist.x = this.screenWidth - this.protagonist.radius;
+    }
+  }
+
+  controlDown(): void {
+    this.vy = this.setMovingVelocity();
+    if (this.protagonist.y + this.protagonist.radius >= this.screenHeight) {
+      this.protagonist.y = this.screenHeight - this.protagonist.radius;
+    }
+  }
+
+  stopX(): void {
+    this.vx = 0;
+  }
+
+  stopY(): void {
+    this.vy = 0;
+  }
+
+  toggleSlow(enable: boolean): void {
+    this.isSlow = enable;
+  }
+}
